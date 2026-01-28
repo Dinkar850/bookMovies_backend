27254ac857f2407144fb57ce578219cfc28cb7e5
Dinkar <dinkar.gupta@joshtechnologygroup.com>
2026-01-28 11:53:20 +0530

    DG_A3_03:Movies:Fix:Fixed duplication in querysets and filters, added slug route

--- apps/movie/filters.py
+++ apps/movie/filters.py
@@ -1,14 +1,8 @@
 import django_filters
 
+from apps.core import filters as CoreFilters
+
 from .models import Movie
-
-
-class CharInFilter(django_filters.BaseInFilter, django_filters.CharFilter):
-    pass
-
-
-class NumberInFilter(django_filters.BaseInFilter, django_filters.NumberFilter):
-    pass
 
 
 class MovieFilter(django_filters.FilterSet):
@@ -20,11 +14,13 @@ class MovieFilter(django_filters.FilterSet):
     - **release_date**: greater than or equal to entered date
     """
 
-    genre = CharInFilter(field_name="genre__name", lookup_expr="in", distinct=True)
-    language = CharInFilter(
+    genre = CoreFilters.CharInFilter(
+        field_name="genre__name", lookup_expr="in", distinct=True
+    )
+    language = CoreFilters.CharInFilter(
         field_name="language__name", lookup_expr="in", distinct=True
     )
-    cinema_id = NumberInFilter(
+    cinema_id = CoreFilters.NumberInFilter(
         field_name="slots__cinema_id", lookup_expr="in", distinct=True
     )
     release_date = django_filters.DateFilter(
