5df9934a3642c76952d540fc4dfa77bf49e46b22
Dinkar <dinkar.gupta@joshtechnologygroup.com>
2026-01-31 07:34:04 +0530

    DG_A3_05:Slots:Fix:Updated nested serializers

--- apps/slot/serializers.py
+++ apps/slot/serializers.py
@@ -1,40 +1,9 @@
 from rest_framework import serializers
 
-from apps.cinema import models as CinemaModels
 from apps.cinema import serializers as CinemaSerializers
-from apps.movie import models as MovieModels
+from apps.movie.serializers import MovieNameSerializer
 
 from .models import Slot
-
-# Serializers for nested relationships:
-
-
-class MovieNestedSerializer(serializers.ModelSerializer):
-    """Serializer for nested relation with movies through slots"""
-
-    class Meta:
-        model = MovieModels.Movie
-        fields = ["id", "name"]
-
-
-class CinemaNestedSerializer(serializers.ModelSerializer):
-    """Serializer for nested relation with cinemas through slots"""
-
-    city = serializers.StringRelatedField(read_only=True)
-
-    class Meta:
-        model = CinemaModels.Cinema
-        fields = ["id", "name", "address", "city"]
-
-
-class CinemaDetailsNestedSerializer(CinemaNestedSerializer):
-    """Serializer that inherits `CinemaNestedSerializer` for adding `rows` and `seat_per_row` fields"""
-
-    class Meta(CinemaNestedSerializer.Meta):
-        fields = CinemaNestedSerializer.Meta.fields + ["rows", "seats_per_row"]
-
-
-# Main serializers:
 
 
 class SlotListSerializer(serializers.ModelSerializer):
@@ -45,8 +14,8 @@ class SlotListSerializer(serializers.ModelSerializer):
     - additionally returns slot's id, schedule and price
     """
 
-    movie = MovieNestedSerializer(read_only=True)
-    cinema = CinemaNestedSerializer(read_only=True)
+    movie = MovieNameSerializer(read_only=True)
+    cinema = CinemaSerializers.CinemaListSerializer(read_only=True)
     language = serializers.StringRelatedField(read_only=True)
 
     class Meta:
@@ -63,7 +32,7 @@ class SlotDetailsSerializer(SlotListSerializer):
     - adds `inactive_seats` in that slot that gets set in `SlotDetailsViewSerializer`
     """
 
-    cinema = CinemaDetailsNestedSerializer(read_only=True)
+    cinema = CinemaSerializers.CinemaDetailsSerializer(read_only=True)
     booked_seats = CinemaSerializers.SeatSerializer(many=True, read_only=True)
     inactive_seats = CinemaSerializers.SeatSerializer(
         many=True, read_only=True, source="cinema.inactive_seats"
