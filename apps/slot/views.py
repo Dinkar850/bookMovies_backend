0c51a64a01908c3cdaf91efb9d85d6e9770c2cc0
Dinkar <dinkar.gupta@joshtechnologygroup.com>
2026-01-31 06:24:03 +0530

    DG_A3_05:Slots:Fix:Updation wrt new seat model addition in cinemas

--- apps/slot/views.py
+++ apps/slot/views.py
@@ -4,6 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 
 from apps.booking import models as BookingModels
+from apps.cinema import models as CinemaModels
 
 from .filters import SlotFilter
 from .models import Slot
@@ -24,8 +25,7 @@ class SlotBaseMixin:
         return (
             Slot.objects.filter(is_active=True, schedule__gte=now)
             .order_by("schedule")
-            .select_related("movie", "cinema", "cinema__city", "language")
-            .prefetch_related("movie__language")
+            .select_related("movie", "cinema__city", "language")
         )
 
 
@@ -47,30 +47,51 @@ class SlotDetailsView(SlotBaseMixin, generics.RetrieveAPIView):
     """
     View that is responsible for:
     - returning detailed active slot with schedule greater than current date and time
-    - including cinema's structure (`rows`, `seat_per_row`)
+    - including cinema's structure (`rows`, `seats_per_row`)
     - fetching booked seats for this slot
     """
 
     serializer_class = SlotDetailsSerializer
 
     def get_queryset(self):
-        # queryset for prefetching ordered booking seats from seats in Booking model to be used in `bookings_queryset`
-        booked_seats_queryset = BookingModels.Seat.objects.order_by(
-            "seat_row", "seat_number"
-        )
+        """Populates `inactive_seats` and `booked_seats` attribute for `confirmed_bookings` by using a custom queryset"""
 
-        # queryset for prefetching confirmed seats from bookings in Slot model using the previously defined `booked_seats_queryset` to be used in final queryset
-        confirmed_bookings_queryset = BookingModels.Booking.objects.filter(
+        # queryset for fetching and ordering all seats
+        cinema_seats = CinemaModels.Seat.objects.order_by("seat_row", "seat_number")
+
+        # queryset for filtering confirmed bookings and then fetching only active seats for each booking
+        confirmed_bookings_qs = BookingModels.Booking.objects.filter(
             status=BookingModels.Booking.BookingStatus.BOOKED
         ).prefetch_related(
-            Prefetch("seats", queryset=booked_seats_queryset, to_attr="booked_seats")
+            Prefetch(
+                "seat",
+                queryset=cinema_seats.filter(is_active=True),
+                to_attr="booked_seats",
+            )
         )
 
-        # queryset for retrieving booked seats from slots -> bookings -> seats
         return self.base_queryset().prefetch_related(
+            # prefetch responsible for populating confirmed_bookings attribute in slot object with booked status and also populating the booked_seats attribute for each confirmed_booking
             Prefetch(
                 "bookings",
-                queryset=confirmed_bookings_queryset,
+                queryset=confirmed_bookings_qs,
                 to_attr="confirmed_bookings",
             ),
+            # prefetch responsible for populating inactive_seats attribute in slot object for that cinema
+            Prefetch(
+                "cinema__seats",
+                queryset=cinema_seats.filter(is_active=False),
+                to_attr="inactive_seats",
+            ),
         )
+
+    def get_object(self):
+        """Get the corresponding slot object as referenced by `pk` in `self.get_queryset()` and set the `booked_seats` attribute to be accessed by `SlotDetailsSerializer`"""
+
+        slot = super().get_object()
+
+        slot.booked_seats = [
+            seat for booking in slot.confirmed_bookings for seat in booking.booked_seats
+        ]
+
+        return slot
