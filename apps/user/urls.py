89bf148cb448018bd361dbbe3c2ba72b4aa3e218
Dinkar <dinkar.gupta@joshtechnologygroup.com>
2026-02-03 10:10:40 +0530

    DG_A3_02:User:Added docstrings, token rotation and logout

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import LoginView, RegisterView, TokenRefreshView, UserView
+from .views import LoginView, LogoutView, RegisterView, TokenRefreshView, UserView
 
 urlpatterns = [
     path("auth/register/", RegisterView.as_view(), name="register"),
     path("auth/login/", LoginView.as_view(), name="login"),
+    path("auth/logout/", LogoutView.as_view(), name="logout"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("user/", UserView.as_view(), name="user"),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,8 +1,11 @@
+import contextlib
+
 from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
 from rest_framework import generics, permissions, response, status
-from rest_framework_simplejwt import tokens, views
+from rest_framework.views import APIView
+from rest_framework_simplejwt import exceptions, tokens, views
 
 from .constants import UserErrors, UserMessages
 from .serializers import TokenObtainPairSerializer, UserSerializer, UserUpdateSerializer
@@ -10,7 +13,7 @@ from .serializers import TokenObtainPairSerializer, UserSerializer, UserUpdateSe
 
 def set_refresh_cookie(res, refresh_token):
     """
-    Sets refresh token cookie with max_age derived from SIMPLE_JWT['REFRESH_TOKEN_LIFETIME']
+    Sets refresh token cookie with `max_age` derived from `SIMPLE_JWT['REFRESH_TOKEN_LIFETIME']`
     """
 
     max_age = int(settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"].total_seconds())
@@ -31,6 +34,7 @@ def set_refresh_cookie(res, refresh_token):
 
 def clear_refresh_cookie(res):
     """Clears refresh cookie from response"""
+
     is_prod = not settings.DEBUG
 
     res.delete_cookie(
@@ -42,15 +46,38 @@ def clear_refresh_cookie(res):
 
 class RegisterView(generics.CreateAPIView):
     """
-    Registers new user plus:
-    - Attaches refresh token in HttpOnly cookie for web
-    - Returns access token for registered user in body
+    POST /auth/register/
+
+    Description:
+        - Creates new user account
+        - Generates access token
+        - Sets refresh token in HttpOnly cookie
+
+    Permissions:
+        - AllowAny
+
+    Response:
+        201 Created
+        {
+            "detail": string,
+            "access": string
+        }
+
+    Errors:
+        400 Bad Request:
+            - Validation errors
     """
 
     serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = (permissions.AllowAny,)
 
     def create(self, request, *args, **kwargs):
+        """
+        - Creates new user
+        - Generates and sets new refresh token in HttpOnly cookie
+        - Sets access token in response body
+        """
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
@@ -64,19 +91,43 @@ class RegisterView(generics.CreateAPIView):
         )
 
         set_refresh_cookie(res, str(refresh))
+
         return res
 
 
 class LoginView(views.TokenObtainPairView):
     """
-    Overrides SimpleJWT default:
-    - sets refresh token in HttpOnly cookie for web
-    - returns access token in res body
+    POST /auth/login/
+
+    Description:
+        - Authenticates user credentials
+        - Returns access token
+        - Stores refresh token in HttpOnly cookie
+
+    Permissions:
+        - AllowAny
+
+    Response:
+        200 OK
+        {
+            "detail": string,
+            "access": string
+        }
+
+    Errors:
+        401 Unauthorized:
+            - Authentication credentials were not provided
+            - Invalid or expired token
     """
 
     serializer_class = TokenObtainPairSerializer
 
     def post(self, request, *args, **kwargs):
+        """
+        - Sets refresh token in HttpOnly cookie
+        - Returns access token in response body
+        """
+
         res = super().post(request, *args, **kwargs)
 
         if res.status_code != status.HTTP_200_OK:
@@ -93,18 +144,116 @@ class LoginView(views.TokenObtainPairView):
             status=status.HTTP_200_OK,
         )
         set_refresh_cookie(res, refresh)
+
+        return res
+
+
+class LogoutView(APIView):
+    """
+    POST /auth/logout/
+
+    Description:
+        - Blacklists current refresh token
+        - Clears refresh cookie
+
+    Permissions:
+        - IsAuthenticated
+
+    Response:
+        200 OK
+        {
+            "detail": string
+        }
+
+    Errors:
+        401 Unauthorized:
+            - Authentication credentials were not provided
+            - Invalid or expired token
+    """
+
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+        """
+        - Blacklists the previously issues refresh token
+        - Clears refresh token from HttpOnly cookie
+        """
+
+        refresh = request.COOKIES.get("refresh")
+
+        if refresh:
+            with contextlib.suppress(Exception):
+                tokens.RefreshToken(refresh).blacklist()
+
+        res = response.Response(
+            {"detail": UserMessages.LOGGED_OUT}, status=status.HTTP_200_OK
+        )
+
+        clear_refresh_cookie(res)
+
         return res
 
 
 class UserView(generics.RetrieveUpdateDestroyAPIView):
     """
-    View for User model that:
-    - **[GET]**: returns all user details after verifying ID from received access token
-    - **[PATCH]**: updates user information (phone_number, first_name, last_name)
-    - **[DELETE]**: sets user's active state to false
+    Endpoints for managing current authenticated user
+
+    Permissions:
+        - IsAuthenticated
+
+    Allowed Methods:
+        GET, PATCH, DELETE
+
+
+    GET:
+        - Returns current user details
+
+        Response:
+            200 OK
+            {
+                "id": int,
+                "first_name": string,
+                "last_name": string,
+                "email": string,
+                "phone_number": string
+            }
+
+
+    PATCH:
+        - Updates profile fields
+
+        Request Body:
+            {
+                "first_name": string,
+                "last_name": string,
+                "phone_number": string
+                ...
+            }
+
+        Response:
+            200 OK
+            {
+                "first_name": string,
+                "last_name": string,
+                "phone_number": string
+            }
+
+
+    DELETE:
+        - Sets user inactive
+        - Blacklists refresh token
+        - Clears cookie
+
+        Response:
+            204 No Content
+
+    Errors:
+        401 Unauthorized:
+            - Authentication credentials were not provided
+            - Invalid or expired token
     """
 
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = UserSerializer
     http_method_names = ("get", "patch", "delete")
 
@@ -118,26 +267,68 @@ class UserView(generics.RetrieveUpdateDestroyAPIView):
 
         if self.request.method == "PATCH":
             return UserUpdateSerializer
+
         return UserSerializer
 
     def destroy(self, request, *args, **kwargs):
-        """Responsible for clearing response cookie and deactivating user by setting `is_active` as false"""
+        """
+        - Responsible for clearing response cookie and deactivating user by setting `is_active` as false
+        - Blacklists refresh token of this user
+        - Clears refresh token from HttpOnly cookie
+        """
 
         user = self.get_object()
         user.is_active = False
         user.save(update_fields=["is_active"])
 
+        refresh = request.COOKIES.get("refresh")
+
+        if refresh:
+            with contextlib.suppress(Exception):
+                tokens.RefreshToken(refresh).blacklist()
+
         res = response.Response(status=status.HTTP_204_NO_CONTENT)
         clear_refresh_cookie(res)
+
         return res
 
 
 @method_decorator(csrf_protect, name="dispatch")
 class TokenRefreshView(views.TokenRefreshView):
-    """Custom `TokenRefreshView` that overrides post mixin for accessing refresh token from cookie instead of accessing from body"""
+    """
+    POST /auth/refresh/
+
+    Description:
+        - Reads refresh token from HttpOnly cookie
+        - Issues new access token
+        - Rotates refresh token
+        - Sets new refresh cookie
+
+    Permissions:
+        - AllowAny
+
+    Request Body:
+        {}
+
+    Response:
+        200 OK
+        {
+            "access": string
+        }
+
+    Errors:
+        400 Bad Request:
+            - Missing refresh token
+        401 Unauthorized:
+            - Invalid, blacklisted or expired token
+    """
 
     def post(self, request, *args, **kwargs):
-        """Overrides post mixin for getting refresh token from cookies"""
+        """
+        - Overrides post mixin for accessing refresh token from HttpOnly cookie
+        - Rotates refresh token, also blacklists previously issued refresh token
+        """
+
         refresh = request.COOKIES.get("refresh")
 
         if not refresh:
@@ -151,9 +342,21 @@ class TokenRefreshView(views.TokenRefreshView):
         data["refresh"] = refresh
 
         serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
 
-        return response.Response(
-            serializer.validated_data,
-            status=status.HTTP_200_OK,
-        )
+        # Issues error in case of blacklisted refresh token
+        try:
+            serializer.is_valid(raise_exception=True)
+        except exceptions.TokenError:
+            return response.Response(
+                {"detail": UserErrors.INVALID_REFRESH_TOKEN},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        new_refresh = serializer.validated_data.pop("refresh")
+
+        res = response.Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+        if new_refresh:
+            set_refresh_cookie(res, new_refresh)
+
+        return res
